20aa59fdc71f7cbec769860d8fc23f28310a147b
13487079308@163.com <13487079308@163.com>
2020-08-04 23:25:37 +0800

    add react-router

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@
  * @作者: 陈英杰
  * @Date: 2020-08-01 01:34:25
  * @LastEditors: 陈英杰
- * @LastEditTime: 2020-08-01 02:53:07
+ * @LastEditTime: 2020-08-03 23:33:45
  */
 import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>App</div>;
+  return <div>App1</div>;
 };
 export default App;
 ReactDOM.render(<App />, document.getElementById("root"));
