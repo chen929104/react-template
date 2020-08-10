cf26274f987e2aacc67979514e63db355d0ef7c2
13487079308@163.com <13487079308@163.com>
2020-08-10 22:51:56 +0800

    feat:新建文件夹

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,13 @@
  * @作者: 陈英杰
  * @Date: 2020-08-01 01:34:25
  * @LastEditors: 陈英杰
- * @LastEditTime: 2020-08-03 23:33:45
+ * @LastEditTime: 2020-08-10 22:51:48
  */
 import React from "react";
 import ReactDOM from "react-dom";
 
 const App = () => {
-  return <div>App1</div>;
+  return <div>App</div>;
 };
 export default App;
 ReactDOM.render(<App />, document.getElementById("root"));
