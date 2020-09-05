2f805caaee7b6bf7890be6a4d91259d9ff1a1c09
13487079308@163.com <13487079308@163.com>
2020-09-05 14:19:23 +0800

    updata page

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,14 @@
  * @作者: 陈英杰
  * @Date: 2020-08-01 01:34:25
  * @LastEditors: 陈英杰
- * @LastEditTime: 2020-08-10 22:51:48
+ * @LastEditTime: 2020-08-11 21:31:59
  */
 import React from "react";
 import ReactDOM from "react-dom";
+import Index from "../src/pages/home";
 
 const App = () => {
-  return <div>App</div>;
+  return <Index />;
 };
 export default App;
 ReactDOM.render(<App />, document.getElementById("root"));
